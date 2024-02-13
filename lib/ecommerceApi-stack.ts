d86eff743663ce3d9e93486bf605d9470f76a6c7
Raphael Americano <raphaelamericano@Raphaels-MacBook-Pro.local>
2024-02-12 22:49:11 -0300

    adicionando formatacao de logs da api rest

--- lib/ecommerceApi-stack.ts
+++ lib/ecommerceApi-stack.ts
@@ -11,8 +11,24 @@ interface ECommerceApiStackProps extends cdk.StackProps {
 export class EcommerceApiStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props: ECommerceApiStackProps ){
         super(scope, id, props)
+        const logGroup = new cwlogs.LogGroup(this, "ECommerceApiLogs")
         const api = new apigateway.RestApi(this, "ECommerceApi", {
-            restApiName: "ECommerceApi"
+            restApiName: "ECommerceApi",
+            // cloudWatchRole: true,
+            deployOptions: {
+                accessLogDestination: new apigateway.LogGroupLogDestination(logGroup),
+                accessLogFormat: apigateway.AccessLogFormat.jsonWithStandardFields({
+                    httpMethod: true,
+                    ip: true,
+                    protocol: true,
+                    requestTime: true,
+                    resourcePath: true,
+                    responseLength: true,
+                    status: true,
+                    caller: true,
+                    user: true
+                })
+            }
         })
 
         const productsFetchIntegration = new apigateway.LambdaIntegration(props.productsFetchHandler)
@@ -21,4 +37,3 @@ export class EcommerceApiStack extends cdk.Stack {
         productsResource.addMethod("GET", productsFetchIntegration)
     }   
 }
-
