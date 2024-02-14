7eb6070ca64b62eccf8d024e6fd7d175b91940d0
Raphael Americano <raphaelamericano@Raphaels-MacBook-Pro.local>
2024-02-14 14:29:21 -0300

    dynamo products config

--- lib/productsApp-stack.ts
+++ lib/productsApp-stack.ts
@@ -3,14 +3,26 @@ import * as lambda from "aws-cdk-lib/aws-lambda"
 import * as lambdaNodeJS from "aws-cdk-lib/aws-lambda-nodejs"
 
 import * as cdk from "aws-cdk-lib"
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb"
 
 import { Construct } from "constructs"
 
 export class ProductsAppStack extends cdk.Stack {
     public readonly productsFetchHandler: lambdaNodeJS.NodejsFunction
-
+    public readonly productsDdb: dynamodb.Table
     constructor(scope:Construct, id: string, props?: cdk.StackProps){
         super(scope, id, props)
+        this.productsDdb = new dynamodb.Table(this, "ProductsDdb", {
+            tableName: "products",
+            removalPolicy: cdk.RemovalPolicy.DESTROY,
+            partitionKey: {
+                name: "id",
+                type: dynamodb.AttributeType.STRING
+            },
+            billingMode: dynamodb.BillingMode.PROVISIONED,
+            readCapacity: 1,
+            writeCapacity:1
+        })
 
         this.productsFetchHandler = new lambdaNodeJS.NodejsFunction(this, "ProductsFetchFunction", {
             runtime: lambda.Runtime.NODEJS_18_X,
@@ -22,10 +34,13 @@ export class ProductsAppStack extends cdk.Stack {
             bundling:{
                 minify: true,
                 sourceMap: false
+            },
+            environment:{
+                PRODUCTS_DDB: this.productsDdb.tableName
             }
         })
-    }
 
-    
+        this.productsDdb.grantReadData(this.productsFetchHandler)
+    }
 
 }
