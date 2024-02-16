e3395749a8d0c1c635c69434944988fe31766ec5
Raphael Americano <raphael@raphaelamericano.com.br>
2024-02-16 13:12:18 -0300

    products fetch function with repostory functions

--- lambda/products/productsFetchFunction.ts
+++ lambda/products/productsFetchFunction.ts
@@ -1,28 +1,42 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
+import { ProductRepository } from "/opt/nodejs/productsLayer";
+import { DynamoDB } from "aws-sdk"
+
+const productsDdb = process.env.PRODUCTS_DDB!
+const ddbClient = new DynamoDB.DocumentClient()
+const productRepository = new ProductRepository(ddbClient, productsDdb)
 
 export async function handler(event:APIGatewayProxyEvent, context:Context, callback: any): Promise<APIGatewayProxyResult>{
 
     const lambdaRequestId = context.awsRequestId
     const apiRequestId = event.requestContext.requestId
-    console.log(`API Gateway ReqeustId: ${apiRequestId} - Lambda RequestId: ${lambdaRequestId}`)
+    console.log(`API Gateway RequestId: ${apiRequestId} - Lambda RequestId: ${lambdaRequestId}`)
     const { resource, httpMethod } = event
 
     if(resource === "/products"){
         if(httpMethod === "GET"){
-            console.log("GET")
+            console.log("GET / products")
+            const products = await productRepository.getAllProducts()
             return {
                 statusCode: 200,
-                body: JSON.stringify({
-                    message: "Hello, World! Lambda"
-                })
+                body: JSON.stringify(products)
             }
         }
     } else if(resource === "/products/{id}"){
-        const productId = event.pathParameters!.id
-        console.log(`GET /products/{productId}`)
-        return { 
-            statusCode: 200,
-            body: `GET /products/{productId}`
+        const productId = event.pathParameters!.id as string
+        console.log(`GET /products/${productId}`)
+        try {
+            const product = await productRepository.getProductsById(productId)
+            return { 
+                statusCode: 200,
+                body: JSON.stringify(product)
+            }
+        } catch (error) {
+            console.error((<Error>error).message)
+            return {
+                statusCode: 404,
+                body: (<Error>error).message
+            }
         }
     }
 
