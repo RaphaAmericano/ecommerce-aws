8f1cece034e1d522c4e15c076eb1f84dd0e86044
Raphael Americano <raphael@raphaelamericano.com.br>
2024-02-19 13:36:49 -0300

    add orders app

---
+++ lib/ordersApp-stack.ts
@@ -0,0 +1,65 @@
+import * as lambda from "aws-cdk-lib/aws-lambda"
+import * as lambdaNodeJS from "aws-cdk-lib/aws-lambda-nodejs"
+import * as cdk from "aws-cdk-lib"
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb"
+import * as ssm from "aws-cdk-lib/aws-ssm"
+import { Construct } from "constructs"
+
+interface OrdersAppStackProps extends cdk.StackProps {
+    productsDdb: dynamodb.Table
+}
+
+export class OrdersAppStack extends cdk.Stack {
+    public readonly ordersHandler: lambdaNodeJS.NodejsFunction
+    constructor(scope: Construct, id: string, props: OrdersAppStackProps ){
+        super(scope, id, props)
+
+        const ordersDdb = new dynamodb.Table(this, "OrdersDdb", {
+            tableName: "orders",
+            partitionKey: {
+                name:"pk",
+                type: dynamodb.AttributeType.STRING
+            },
+            sortKey: {
+                name: "sk",
+                type: dynamodb.AttributeType.STRING
+            },
+            billingMode: dynamodb.BillingMode.PROVISIONED,
+            readCapacity: 1,
+            writeCapacity: 1
+        })
+
+        // Orders Layer
+        const ordersLayerArn = ssm.StringParameter.valueForStringParameter(this, "OrdersLayerVersionArn")
+        const ordersLyer = lambda.LayerVersion.fromLayerVersionArn(this, "OrdersLayerVersionArn", ordersLayerArn)
+        // Products Layer 
+        // ? Products Layer
+        const productsLayerArn = ssm.StringParameter.valueForStringParameter(this, "ProductsLayerVersionArn")
+        const productsLayer = lambda.LayerVersion.fromLayerVersionArn(this, "ProductsLayerVersionArn", productsLayerArn)
+
+        this.ordersHandler = new lambdaNodeJS.NodejsFunction(this, "OrdersFunction", {
+            runtime: lambda.Runtime.NODEJS_20_X,
+            functionName: "OrdersFunction",
+            entry: "lambda/orders/ordersFunction.ts",
+            handler: "handler",
+            memorySize: 512,
+            timeout: cdk.Duration.seconds(5),
+            bundling:{
+                minify: true, 
+                sourceMap: false
+            },
+            environment:{
+                PRODUCTS_DDB: props.productsDdb.tableName,
+                ORDERS_DDB: ordersDdb.tableName
+            },
+            layers: [ordersLyer, productsLayer],
+            tracing: lambda.Tracing.ACTIVE,
+            insightsVersion: lambda.LambdaInsightsVersion.VERSION_1_0_119_0
+        })
+
+        ordersDdb.grantReadWriteData(this.ordersHandler)
+
+        props.productsDdb.grantReadData(this.ordersHandler)
+        
+    }
+}
