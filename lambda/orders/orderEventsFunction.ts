f12c1766739995170f6e6103f36cb8249a14b3d7
Raphael Americano <raphael@raphaelamericano.com.br>
2024-02-22 11:22:48 -0300

    add order events function body

---
+++ lambda/orders/orderEventsFunction.ts
@@ -0,0 +1,16 @@
+import { DynamoDB } from "aws-sdk"
+import * as AWSXRay from "aws-xray-sdk"
+import { OrderEventRepository } from "/opt/nodejs/orderEventsRepositoryLayer"
+import { Context, SNSEvent } from "aws-lambda"
+
+AWSXRay.captureAWS(require("aws-sdk"))
+
+const eventsDdb = process.env.EVENTS_DDB!
+
+const ddbClient = new DynamoDB.DocumentClient()
+const orderEventsRepository = new OrderEventRepository(ddbClient, eventsDdb)
+
+export async function handler(event: SNSEvent, context: Context): Promise<void> {
+    
+    return 
+}
