5c808c2b5cc4cffd9f508840d937a8eaf235e764
Raphael Americano <raphael@raphaelamericano.com.br>
2024-02-23 14:43:58 -0300

    dlq simulated comment

--- lambda/orders/orderEmailsFunction.ts
+++ lambda/orders/orderEmailsFunction.ts
@@ -4,10 +4,17 @@ import * as AWSXRay from "aws-xray-sdk"
 AWSXRay.captureAWS(require("aws-sdk"))
 
 export async function handler(event:SQSEvent, context: Context): Promise<void>{
+    
+    // info: to test DLQ
+    // console.log('Non valid event type')
+    // throw 'Non valid event type'
+    
     event.Records.forEach((record) => {
         console.log(record)
         const body = JSON.parse(record.body)
         console.log(body)
     })
+
+
     return
 }
