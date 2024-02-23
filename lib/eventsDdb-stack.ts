9a0aabfb1c9c6fc29ca3b1ad5f12aa91c8373d24
Raphael Americano <raphael@raphaelamericano.com.br>
2024-02-23 17:24:06 -0300

    change billing mode of events ddb

--- lib/eventsDdb-stack.ts
+++ lib/eventsDdb-stack.ts
@@ -18,31 +18,35 @@ export class EventsDdbStack extends cdk.Stack {
                 type: dynamodb.AttributeType.STRING
             },
             timeToLiveAttribute: "ttl",
-            billingMode: dynamodb.BillingMode.PROVISIONED,
-            readCapacity: 1,
-            writeCapacity: 1
-        })
-
-        const readScale = this.table.autoScaleReadCapacity({
-            maxCapacity: 2,
-            minCapacity: 1
-        })
-        readScale.scaleOnUtilization({
-            targetUtilizationPercent: 50,
-            scaleInCooldown: cdk.Duration.seconds(60),
-            scaleOutCooldown: cdk.Duration.seconds(60)
+            billingMode: dynamodb.BillingMode.PAY_PER_REQUEST
+            // info: auto scalling
+            // billingMode: dynamodb.BillingMode.PROVISIONED,
+            // readCapacity: 1,
+            // writeCapacity: 1
         })
 
 
-        const writeScale = this.table.autoScaleWriteCapacity({
-            maxCapacity: 4,
-            minCapacity: 1
-        })
-        writeScale.scaleOnUtilization({
-            targetUtilizationPercent: 30,
-            scaleInCooldown:cdk.Duration.seconds(60),
-            scaleOutCooldown: cdk.Duration.seconds(60)
-        })
+        // info: auto scalling
+        // const readScale = this.table.autoScaleReadCapacity({
+        //     maxCapacity: 2,
+        //     minCapacity: 1
+        // })
+        // readScale.scaleOnUtilization({
+        //     targetUtilizationPercent: 50,
+        //     scaleInCooldown: cdk.Duration.seconds(60),
+        //     scaleOutCooldown: cdk.Duration.seconds(60)
+        // })
+
+
+        // const writeScale = this.table.autoScaleWriteCapacity({
+        //     maxCapacity: 4,
+        //     minCapacity: 1
+        // })
+        // writeScale.scaleOnUtilization({
+        //     targetUtilizationPercent: 30,
+        //     scaleInCooldown:cdk.Duration.seconds(60),
+        //     scaleOutCooldown: cdk.Duration.seconds(60)
+        // })
     }   
 
 }
