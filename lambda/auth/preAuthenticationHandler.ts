d8e3c87734f633f6743563cc4bf6baef74458a00
Raphael Americano <raphael@raphaelamericano.com.br>
2024-03-15 09:07:15 -0300

    add pre auth lambda

--- lambda/auth/preAuthenticationHandler.ts
+++ lambda/auth/preAuthenticationHandler.ts
@@ -3,7 +3,9 @@ import { Callback, Context, PreAuthenticationTriggerEvent } from "aws-lambda";
 export async function handler(event: PreAuthenticationTriggerEvent, context: Context, callback: Callback): Promise<void> {
     console.log(event)
 
-
-    callback(null, event)
-
+    if(event.request.userAttributes.email === "usuario@bloqueado.com.br"){
+        callback("this user is blocked. Reason: PAYMENT", event )
+    } else {
+        callback(null, event)
+    }
 }
